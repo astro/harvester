ee880761e6bda5adfe0af1560207fac344c6ddd3
astro <astro@c1e8ea29-1c23-0410-9d2e-ab085942b101>
2006-11-27 17:42:08 +0000

    Kick tmpl, utilize XSLT! (only templates/index.html provided so far)
    git-svn-id: svn+ssh://svn.c3d2.de/harvester/trunk/harvester@3 c1e8ea29-1c23-0410-9d2e-ab085942b101

--- generate.rb
+++ generate.rb
@@ -3,136 +3,84 @@
 require 'dbi'
 require 'yaml'
 require 'rexml/document'
+require 'xml/xslt'
 require 'time'
 
-class VarBinding < Hash
-  def [](k)
-    if k =~ /^snip\((.+),(\d+)\)$/
-      var, length = $1, $2.to_i
 
-      value = self[var]
-      if value.size > length
-        "#{value[0..length-3]}..."
-      else
-        value
-      end
-    elsif k=~ /^strip\((.+)\)$/
-      value = self[$1]
+class XSLTFunctions
+  FUNC_NAMESPACE = 'http://astroblog.spaceboyz.net/harvester/xslt-functions'
 
-      #puts "Strip:\n<<< #{value.inspect}\n>>> #{value.gsub(/<.+?>/, '').strip.inspect}"
-      value.gsub(/<.+?>/, '').strip
-    else
-      super
-    end
+  def initialize(dbi)
+    @dbi = dbi
+    %w(collection-items feed-items item-description).each { |func|
+      XML::XSLT.extFunction(func, FUNC_NAMESPACE, self)
+    }
   end
-end
 
-BLOG_ATTR = %w(title link description)
-ITEM_ATTR = %w(title link description date)
+  def generate_root
+    root = REXML::Element.new('collections')
+    @dbi.select_all("SELECT collection FROM sources GROUP BY collection") { |name,|
+      collection = root.add(REXML::Element.new('collection'))
+      collection.attributes['name'] = name
+      @dbi.select_all("SELECT rss,title,link,description FROM sources WHERE collection=?", name) { |rss,title,link,description|
+        feed = collection.add(REXML::Element.new('feed'))
+        feed.add(REXML::Element.new('rss')).text = rss
+        feed.add(REXML::Element.new('title')).text = title
+        feed.add(REXML::Element.new('link')).text = link
+        feed.add(REXML::Element.new('description')).text = description
+      }
+    }
 
-def escape(s)
-  s.gsub(/&/, '&amp;').gsub(/"/, '&quot;').gsub(/</, '&lt;').gsub(/>/, '&gt;')
-end
+    root
+  end
 
-def parse_template_element(dbi, element, binding=VarBinding.new, attributes={})
-  s = ''
-  
-  element.each { |child|
-    if child.kind_of? REXML::Element
-      
-      if child.prefix == 'tmpl'
-        case child.name
-          when 'print'
-            value = binding[child.attributes['var']]
-            raise "Unbound variable #{child.attributes['var']}" unless value
-            s += (child.attributes['escape'] == 'false' ? value : escape(value))
-          when 'param'
-            value = binding[child.attributes['var']]
-            raise "Unbound variable #{child.attributes['var']}" unless value
+  def collection_items(collection, max=23)
+    items = REXML::Element.new('items')
+    @dbi.select_all("SELECT items.title,items.date,items.link,items.rss FROM items,sources WHERE items.rss=sources.rss AND sources.collection=? ORDER BY items.date DESC LIMIT ?", collection, max.to_i) { |title,date,link,rss|
+      item = items.add(REXML::Element.new('item'))
+      item.add(REXML::Element.new('title')).text = title
+      item.add(REXML::Element.new('date')).text = date
+      item.add(REXML::Element.new('link')).text = link
+      item.add(REXML::Element.new('rss')).text = rss
+    }
 
-            childattributes = attributes.merge({child.attributes['param'] => (child.attributes['escape'] == 'false' ? value : escape(value))})
-            s += parse_template_element(dbi, child, binding, childattributes)
-          when 'iter-blog'
-            # Prepare query
-            sorting = ''
-            sorting_order = (child.attributes['reverse'] == 'true' ? 'DESC' : 'ASC')
-            sorting = "ORDER BY LOWER(#{child.attributes['sort']}) #{sorting_order}" if BLOG_ATTR.include? child.attributes['sort']
-            limit = "LIMIT #{child.attributes['max']}" if child.attributes['max'] =~ /^\d+$/
+    items
+  end
 
-            blogs = dbi.execute "SELECT #{BLOG_ATTR.join(', ')} FROM sources WHERE collection=? #{sorting} #{limit}",
-              child.attributes['collection']
+  def feed_items(rss, max=23)
+    items = REXML::Element.new('items')
+    @dbi.select_all("SELECT title,date,link FROM items WHERE rss=? ORDER BY date DESC LIMIT ?", rss, max.to_i) { |title,date,link|
+      item = items.add(REXML::Element.new('item'))
+      item.add(REXML::Element.new('title')).text = title
+      item.add(REXML::Element.new('date')).text = date
+      item.add(REXML::Element.new('link')).text = link
+    }
 
-            # Fetch row-by-row
-            while row = blogs.fetch_hash do
-              childbinding = binding.dup
-              BLOG_ATTR.each { |column|
-                childbinding[child.attributes[column]] = row[column].to_s if child.attributes[column]
-              }
-              s += parse_template_element(dbi, child, childbinding, attributes)
-            end
-          when 'iter-item'
-            # Prepare query
-            sorting = ''
-            sorting_order = (child.attributes['reverse'] == 'true' ? 'DESC' : 'ASC')
-            sorting = "ORDER BY LOWER(#{child.attributes['sort']}) #{sorting_order}" if ITEM_ATTR.include? child.attributes['sort']
-            limit = "LIMIT #{child.attributes['max']}" if child.attributes['max'] =~ /^\d+$/
-            blogcondition = 'AND sources.title LIKE ?'
-            blog = '%'
-            if child.attributes['blog']
-              blogcondition = 'AND sources.title=?'
-              blog = binding[child.attributes['blog']]
-              raise "Unbound variable #{child.attributes['blog']}" unless blog
-            end
+    items
+  end
 
-            items = dbi.execute "SELECT #{ITEM_ATTR.collect{|a|'items.'+a}.join(', ')}, #{BLOG_ATTR.collect{|a|'sources.'+a+' AS blog'+a}.join(', ')} FROM items, sources WHERE items.rss=sources.rss AND sources.collection=? #{blogcondition} #{sorting} #{limit}",
-              child.attributes['collection'], blog
-            # Fetch row-by-row
-
-            while row = items.fetch_hash
-              childbinding = binding.dup
-              ITEM_ATTR.each { |column|
-                childbinding[child.attributes[column]] = (column == 'date' ?
-                  row[column].to_time.strftime(binding['strftime'].to_s) :
-                  row[column].to_s) if child.attributes[column]
-              }
-              childbinding[child.attributes['rssdate']] = row["date"].to_time.iso8601 if child.attributes['rssdate']
-              BLOG_ATTR.each { |column2|
-                column = "blog#{column2}"
-                childbinding[child.attributes[column]] = row[column].to_s if child.attributes[column]
-              }
-              s += parse_template_element(dbi, child, childbinding, attributes)
-            end
-          else
-            raise "Unknown template command: #{child.name}"
-        end
-        
-      else
-        childname = (child.prefix == '' ? child.name : "#{child.prefix}:#{child.name}")
-        childattributes = attributes.collect { |k,v| " #{k}=\"#{v}\""}.to_s
-        child.attributes.each { |k,v| childattributes += " #{k}=\"#{v}\""}
-        #childattributes = attributes.merge(child.attributes).collect { |k,v| " #{k}=\"#{v}\"" }
-        s += "<#{childname}#{childattributes}>" + parse_template_element(dbi, child, binding) + "</#{childname}>"
-      end
-      
-    else
-      s += child.to_s
-    end
-  }
-  
-  s
+  def item_description(rss, item_link)
+    @dbi.select_all("SELECT description FROM items WHERE rss=? AND link=?", rss, item_link) { |desc,|
+      return desc
+    }
+    ''
+  end
 end
 
 
 config = YAML::load File.new('config.yaml')
-dbi = DBI::connect(config['db']['driver'], config['db']['user'], config['db']['password'])
+f = XSLTFunctions.new(DBI::connect(config['db']['driver'], config['db']['user'], config['db']['password']))
+
+xslt = XML::XSLT.new
+xslt.xml = f.generate_root.to_s
 
 templatedir = config['settings']['templates']
 outputdir = config['settings']['output']
 Dir.foreach(templatedir) { |templatefile|
   next if templatefile =~ /^\./
+  next if templatefile != 'index.html'
 
   puts "Processing #{templatefile}"
-  template = REXML::Document.new(File.new("#{templatedir}/#{templatefile}"))
-
-  File.new("#{outputdir}/#{templatefile}", 'w').write parse_template_element(dbi, template, VarBinding.new.merge(config['vars']))
+  xslt.xsl = "#{templatedir}/#{templatefile}"
+  File::open("#{outputdir}/#{templatefile}", 'w') { |f| f.write(xslt.serve) }
 }

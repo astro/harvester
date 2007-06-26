bfdd7aab24fd52e623a69c5a9da8c6e52a3e795b
astro <astro@c1e8ea29-1c23-0410-9d2e-ab085942b101>
2007-06-26 03:18:14 +0000

    A cool chart
    git-svn-id: svn+ssh://svn.c3d2.de/harvester/trunk/harvester@34 c1e8ea29-1c23-0410-9d2e-ab085942b101

---
+++ chart.rb
@@ -0,0 +1,59 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+require 'dbi'
+require 'yaml'
+require 'gruff'
+
+config = YAML::load File.new('config.yaml')
+timeout = config['settings']['timeout'].to_i
+sizelimit = config['settings']['size limit'].to_i
+dbi = DBI::connect(config['db']['driver'], config['db']['user'], config['db']['password'])
+
+class StatsPerCollection
+  attr_reader :days
+
+  def initialize
+    @collections = {}
+    @days = []
+  end
+
+  def add_one(collection, day)
+    @days << day unless @days.index(day)
+
+    c = @collections[collection] || {}
+    c[day] = (c[day] || 0) + 1
+    @collections[collection] = c
+  end
+
+  def each
+    @collections.each { |n,c|
+      v = []
+      @days.each { |d|
+        v << c[d].to_i
+      }
+
+      yield n, v
+    }
+  end
+end
+
+c = StatsPerCollection.new
+dbi.select_all("select date(items.date) as date,sources.collection from items left join sources on sources.rss=items.rss where date > now() - interval '7 days' order by date") { |date,collection|
+  c.add_one(collection, date.day)
+}
+
+g = Gruff::Line.new(400)
+g.title = "Harvested items"
+
+c.each(&g.method(:data))
+
+labels = {}
+c.days.each_with_index do |d,i|
+  labels[i] = d.to_s
+end
+g.labels = labels
+
+g.write('chart.png')
+
+
